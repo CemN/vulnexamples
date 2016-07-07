8ac311e94813f0b9394780c0dedd841604a6dfef
Cem N <cem@crow.local>
2016-07-07 08:30:06 +0200

    first commit

---
+++ Example.java
@@ -0,0 +1,16 @@
+import javax.script.*;
+
+public class Example {
+    
+    public static void main(String[] args) {
+    
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+            System.out.println(args[0]);
+            engine.eval("print('"+ args[0] + "')");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
